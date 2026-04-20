9acd87c486005daa1a75938aca8eeb969854e75d
gorintlapavanprasad <gorintlapavanprasad@gmail.com>
2026-04-20 09:40:42 -0700

    ios commit

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -26,15 +26,9 @@ class DefaultFirebaseOptions {
       case TargetPlatform.android:
         return android;
       case TargetPlatform.iOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for ios - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return ios;
       case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return macos;
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
@@ -59,4 +53,23 @@ class DefaultFirebaseOptions {
     projectId: 'cardiocare-quest',
     storageBucket: 'cardiocare-quest.firebasestorage.app',
   );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'AIzaSyCi6r-0kmRe1RYPtkvEb6COU_0yN0jBt7E',
+    appId: '1:74239338720:ios:990464b48bef9a56ce8885',
+    messagingSenderId: '74239338720',
+    projectId: 'cardiocare-quest',
+    storageBucket: 'cardiocare-quest.firebasestorage.app',
+    iosBundleId: 'com.nau.cardiocarequest',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyCi6r-0kmRe1RYPtkvEb6COU_0yN0jBt7E',
+    appId: '1:74239338720:ios:990464b48bef9a56ce8885',
+    messagingSenderId: '74239338720',
+    projectId: 'cardiocare-quest',
+    storageBucket: 'cardiocare-quest.firebasestorage.app',
+    iosBundleId: 'com.nau.cardiocarequest',
+  );
+
 }
